55cb9221e8a9a8243fe3835259a2e803868ea162
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-07-14 11:50:14 -0300

    mudando barra de pesquisa

--- js/barraPesquisa.js
+++ js/barraPesquisa.js
@@ -1,10 +1,21 @@
-let input = document.querySelector('[data-input]');
+//INPUT//
+let input = document.querySelector('.input_search');
+//ISSO PERCORRE O QUE FOI ESCRITO NO INPUT//
+input.addEventListener('keyup', function(e){
+    input = e.target.value;
+})
+
+//BOTÃO DE PESQUISA//
 let inputLink = document.querySelector('[data-input-link]');
+
+//PERCORRE OS PRODUTOS//
 let produtos = document.querySelectorAll('[data-nome-produto]');
 
+console.log(produtos);
+
+//FUNÇÃO QUE VAI ACIONAR O BOTÃO//
 inputLink.addEventListener('click', function(e) {
     e.preventDefault();
-    inputLink.innerHTML = '';
-});
 
-console.log(produtos);
+    console.log(input);
+});

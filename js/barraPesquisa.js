be9508fe79cf2ce78de430e4ae75e03122c51fed
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-07-15 11:26:00 -0300

    barra de pequisa quase funcional

--- js/barraPesquisa.js
+++ js/barraPesquisa.js
@@ -1,21 +1,23 @@
-//INPUT//
-let input = document.querySelector('.input_search');
-//ISSO PERCORRE O QUE FOI ESCRITO NO INPUT//
-input.addEventListener('keyup', function(e){
-    input = e.target.value;
-})
+// FUNÇÃO RESPONSAVEL POR PESQUISAR OS ITENS//
+function pesquisa(){
+    let input = document.querySelector('.input_search').value
+    input = input.toLowerCase();
+    let produtos = document.querySelectorAll('[data-nome-produto]');
+//FOR PARA PERCORRER OS NOMES DOS PRODUTOS//
+    for (i = 0; i < produtos.length; i++) { 
+        //ESTE IF É PARA QUANDO O PRODUTO NÃO FOR O ENCONTRADO, ELE TBM DIMINUI A LETRA DOS ITENS PARA PODER ENCONTRAR CASO O USUÁRIO ESCREVA TUDO MINUSCULO//
+        if (!produtos[i].innerHTML.toLowerCase().includes(input)) {
+            produtos[i].style.backgroundColor = '';
+        }
+        //ESTE ELSE É PARA QUANDO O PRODUTO FOR ENCONTRADO
+        else {
+            produtos[i].style.backgroundColor = 'black';           
+        }
+    }
+    return pesquisa;
+}
 
-//BOTÃO DE PESQUISA//
-let inputLink = document.querySelector('[data-input-link]');
-
-//PERCORRE OS PRODUTOS//
-let produtos = document.querySelectorAll('[data-nome-produto]');
-
-console.log(produtos);
-
-//FUNÇÃO QUE VAI ACIONAR O BOTÃO//
-inputLink.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    console.log(input);
-});
+//FUNÇÃO PARA REDIRECINAR OS USUÁRIOS PARA AS TELAS DE PRODUTOS//
+function redirecionar(){
+    window.location.href = 'produtosHome.html'
+}

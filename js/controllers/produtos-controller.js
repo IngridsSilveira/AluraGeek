73d3b22ec08d19003a0eb1a795a8024ede9c7b8d
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-07-29 08:21:06 -0300

    vou estudar mais e depois termino

--- js/controllers/produtos-controller.js
+++ js/controllers/produtos-controller.js
@@ -1,16 +1,18 @@
 import { produtosServices } from "../services/produto-services.js";
 
-const novoProduto = (nome, price, imageUrl) => {
+const novoProduto = (nome, price, imageUrl, id, categoria) => {
     const card = document.createElement("div");
     const conteudo = `
-            <div class="produtos_home">
+        <div class="card" ${categoria}>
+            <div class="produtos_home" id="${id}">
             <img class="images_home" src="${imageUrl}">
             <h3 class="produtos_titulo" data-nome-produto>${nome}</h3>
             <p class="preco_home">${price}</p>
             <a href="#" class="links link">Ver produto</a>
             </div>
+        </div>
     `
-    card.innerHTML = conteudo
+    card.innerHTML = conteudo;
     return card
 }
 
@@ -21,7 +23,7 @@ const render = async () => {
         const listaProdutos = await produtosServices.listaProdutos()
         listaProdutos.forEach(elemento =>{
             for(let i = 0; i < produtos.length; i++){
-                produtos[i].appendChild(novoProduto(elemento.nome, elemento.price, elemento.imageUrl));
+                    produtos[i].appendChild(novoProduto(elemento.nome, elemento.price, elemento.imageUrl, elemento.id, elemento.categoria));
             }
         });
     }
